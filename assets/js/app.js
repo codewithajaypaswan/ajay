ab44e525eccbc6c6ecf8d7cb95eb88c15f68b446
codewithajaypaswan <60291173+codewithajaypaswan@users.noreply.github.com>
2021-01-25 09:25:39 +0530

    Add files via upload
    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let header = $(`
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<a class="navbar-brand" href="index.html">Ajay Paswan </a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse " id="navbarSupportedContent">
+  <ul class="navbar-nav ml-auto">
+   <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+   <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>
+   <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
+   <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>
+  </ul>
+</div>
+</nav>`);
+
+let footer = $(`
+`);
+
+
+
+// Window Loads
+$(function () {
+	let bodyElement = $(`body`);
+	bodyElement.prepend(header);
+	bodyElement.append(footer);
+
+});
